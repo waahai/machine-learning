0fcd1071ca81deedb6dfdebc44d8cbf5d642dfc3
Hai Wang <cdwanghai1@jd.com>
2016-10-25 15:28:17 +0800

    feature normalize

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
@@ -106,6 +106,9 @@ fprintf('\n');
 % not need to be normalized.
 price = 0; % You should change this
 
+predict_X = [1650, 3]
+scaled_X = (predict_X - mu)./sigma
+price = theta' * [1, scaled_X]'
 
 % ============================================================
 
@@ -150,7 +153,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = theta' * [1, 1650, 3]'
 
 % ============================================================
 
