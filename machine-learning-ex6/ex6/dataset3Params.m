229cd12a868763c87623176b5ac27d9a8917229f
Hai Wang <cdwanghai1@jd.com>
2016-11-10 17:17:34 +0800

    fill the result directly

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,26 +22,26 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
-params = [0.01 0.03 0.1 0.3 1 3 10 30]';
-len = length(params);
-errors = zeros(length(params)^2, 1);
-for i = 1:len
-  for j = 1:len
-    C = params(i);
-    sigma = params(j);
-    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-    predictions = svmPredict(model, Xval);
-    errors( (i-1)*len + j ) = mean(double(predictions ~= yval));
-  end
-end
-
-[error_min error_min_index] = min(errors);
-C = params( ceil(error_min_index/len) );
-if( mod(error_min_index, len) == 0)
-  sigma = params(8);
-else
-  sigma = params( mod(error_min_index, len) );
-endif
+% params = [0.01 0.03 0.1 0.3 1 3 10 30]';
+% len = length(params);
+% errors = zeros(length(params)^2, 1);
+% for i = 1:len
+%   for j = 1:len
+    % C = params(i);
+    % sigma = params(j);
+    % model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    % predictions = svmPredict(model, Xval);
+%     errors( (i-1)*len + j ) = mean(double(predictions ~= yval));
+%   end
+% end
+%
+% [error_min error_min_index] = min(errors);
+% C = params( ceil(error_min_index/len) )
+% if( mod(error_min_index, len) == 0)
+%   sigma = params(8)
+% else
+%   sigma = params( mod(error_min_index, len) )
+% endif
 
 % =========================================================================
 
