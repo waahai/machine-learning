dcb35a69d603a30126a96ea5f19dfc4b9b7263c8
Hai Wang <cdwanghai1@jd.com>
2016-11-10 16:49:39 +0800

    implement params search

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,19 +15,33 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+params = [0.01 0.03 0.1 0.3 1 3 10 30]';
+len = length(params);
+errors = zeros(length(params)^2, 1);
+for i = 1:len
+  for j = 1:len
+    C = params(i);
+    sigma = params(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    errors( (i-1)*len + j ) = mean(double(predictions ~= yval));
+  end
+end
 
-
-
-
-
-
+[error_min error_min_index] = min(errors);
+C = params( ceil(error_min_index/len) );
+if( mod(error_min_index, len) == 0)
+  sigma = params(8);
+else
+  sigma = params( mod(error_min_index, len) );
+endif
 
 % =========================================================================
 
